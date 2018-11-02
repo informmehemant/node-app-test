77a888b51086c3c2b75cda8f4d8edaa5c17f397d
Hemant <info90.hemant@gmail.com>
2018-11-02 08:55:04 +0530

    heroku setup done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
-
+const port = process.env.PORT || 3000;
 app.set('view engine','hbs');
 // middleware
 hbs.registerPartials(__dirname + '/views/partials');
@@ -53,6 +53,6 @@ app.get('/bad',(req, res) =>{
     errorMsg: 'unable to handle request'
    });
 });
-app.listen(3000,() => {
-    console.log('server is up on port 3000')
+app.listen(port,() => {
+    console.log(`server is up on port  ${port}`);
 });
